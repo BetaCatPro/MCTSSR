35433bd53ca5db8a9fbeb9976a260efc0c2de80e
BetaCatPro <zj20162325@163.com>
2023-11-28 21:11:07 +0800

    file: delete other files

--- 新建文件夹/README.md
+++
@@ -1,1 +0,0 @@
-## 度量空间中一致性 Teacher-Student Model
--- 新建文件夹/utils/tools.py
+++
@@ -1,8 +0,0 @@
-def to_csv(df, path, index=False, index_label='index'):
-    """
-    将DataFrame转换为csv文件
-    :param df: 目标 DataFrame
-    :param path: 保存路径
-    :return: None
-    """
-    df.to_csv(path, index=index, index_label=index_label)
